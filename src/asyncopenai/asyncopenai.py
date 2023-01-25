e44ce1cc1b2c26ccaf8ba1f4f7dba016428df64a
Karn <karn@internet>
2023-01-25 07:13:21 +0000

    add additional exception handling

--- src/asyncopenai/asyncopenai.py
+++ src/asyncopenai/asyncopenai.py
@@ -4,6 +4,7 @@ import aiohttp
 import functools
 import json
 
+
 @functools.lru_cache(maxsize=None)
 def get_api_key():
     with open("openai_secrets.json") as f:
@@ -13,26 +14,38 @@ def get_api_key():
 
 async def request(method, url, json=None, headers=None):
     async with aiohttp.ClientSession() as session:
-        if method == 'GET':
+        if method == "GET":
             async with session.get(url, json=json, headers=headers) as response:
                 return await response.json()
-        elif method == 'POST':
+        elif method == "POST":
             async with session.post(url, json=json, headers=headers) as response:
                 return await response.json()
+
 
 async def openai_request(method, url, params=None, headers=None):
     headers["Authorization"] = "Bearer " + get_api_key()
     return await request(method, url, params, headers)
 
+
 async def list_models():
     url = "https://api.openai.com/v1/models"
     return await openai_request("GET", url)
+
 
 async def get_model(model):
     url = "https://api.openai.com/v1/models/" + model
     return await openai_request("GET", url)
 
-async def create_completion(prompt, engine="text-davinci-003", max_tokens=200, temperature=0, top_p=1, frequency_penalty=0, presence_penalty=0):
+
+async def create_completion(
+    prompt,
+    engine="text-davinci-003",
+    max_tokens=200,
+    temperature=0,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+):
     url = "https://api.openai.com/v1/completions"
     headers = {"Content-Type": "application/json"}
     json = {
@@ -42,13 +55,14 @@ async def create_completion(prompt, engine="text-davinci-003", max_tokens=200, t
         "temperature": temperature,
         "top_p": top_p,
         "frequency_penalty": frequency_penalty,
-        "presence_penalty": presence_penalty
+        "presence_penalty": presence_penalty,
     }
     try:
         r = await openai_request("POST", url, json, headers)
     except aiohttp.client_exceptions.ContentTypeError:
         return None
     return r
+
 
 async def create_embedding(text, model="text-embedding-ada-002"):
     url = "https://api.openai.com/v1/embeddings"
@@ -59,6 +73,9 @@ async def create_embedding(text, model="text-embedding-ada-002"):
     }
     try:
         r = await openai_request("POST", url, json, headers)
-    except aiohttp.client_exceptions.ContentTypeError:
+    except (
+        aiohttp.client_exceptions.ContentTypeError,
+        aiohttp.client_exceptions.ClientConnectorError,
+    ):
         return None
     return r
